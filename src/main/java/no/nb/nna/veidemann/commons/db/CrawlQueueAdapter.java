58593fcc221e4c682656d91be199c2daace17791
johnerikhalse <johnerikhalse@gmail.com>
2019-02-26 16:03:54 +0100

    Changed CrawlQueueAdapterApi to allow for more efficient queue handling

--- src/main/java/no/nb/nna/veidemann/commons/db/CrawlQueueAdapter.java
+++ src/main/java/no/nb/nna/veidemann/commons/db/CrawlQueueAdapter.java
@@ -11,17 +11,13 @@ public interface CrawlQueueAdapter {
 
     boolean uriNotIncludedInQueue(QueuedUri qu, Timestamp since) throws DbException;
 
-    /**
-     * Get the first URI wich is ready to be fetched for a CrawlHostGroup.
-     *
-     * @param crawlHostGroup the CrawlHostGroup for which a URI is requested
-     * @return an Optional containing the next URI to be fetched or empty if none are ready yet.
-     */
-    FutureOptional<QueuedUri> getNextQueuedUriToFetch(CrawlHostGroup crawlHostGroup) throws DbException;
+    CrawlQueueFetcher getCrawlQueueFetcher();
+
+//    FutureOptional<QueuedUri> getNextQueuedUriToFetch(CrawlHostGroup crawlHostGroup) throws DbException;
 
     QueuedUri addToCrawlHostGroup(QueuedUri qUri) throws DbException;
 
-    FutureOptional<CrawlHostGroup> borrowFirstReadyCrawlHostGroup() throws DbException;
+//    FutureOptional<CrawlHostGroup> borrowFirstReadyCrawlHostGroup() throws DbException;
 
     void releaseCrawlHostGroup(CrawlHostGroup crawlHostGroup, long nextFetchDelayMs) throws DbException;
 
