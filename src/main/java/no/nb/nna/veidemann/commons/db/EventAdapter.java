029cde31d02ed10d07ac361e5f734b67a063694e
Andreas Børsheim <andreas.borsheim@nb.no>
2019-03-15 12:39:56 +0100

    wip: add event db adapter

---
+++ src/main/java/no/nb/nna/veidemann/commons/db/EventAdapter.java
@@ -0,0 +1,21 @@
+package no.nb.nna.veidemann.commons.db;
+
+import no.nb.nna.veidemann.api.eventhandler.v1.DeleteResponse;
+import no.nb.nna.veidemann.api.eventhandler.v1.ListCountResponse;
+import no.nb.nna.veidemann.api.eventhandler.v1.UpdateRequest;
+import no.nb.nna.veidemann.api.eventhandler.v1.UpdateResponse;
+
+public interface EventAdapter {
+
+    EventObject getEventObject(EventRef request) throws DbException;
+
+    ChangeFeed<EventObject> listEventObjects(no.nb.nna.veidemann.api.eventhandler.v1.ListRequest request) throws DbException;
+
+    ListCountResponse countEventObjects(no.nb.nna.veidemann.api.eventhandler.v1.ListRequest request) throws DbException;
+
+    EventObject saveEventObject(EventObject object) throws DbException;
+
+    UpdateResponse updateEventObject(UpdateRequest request) throws DbException;
+
+    DeleteResponse deleteEventObject(EventObject object) throws DbException;
+}
